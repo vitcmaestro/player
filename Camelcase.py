76733ddad18341e3bc4692a8fe6bc81002c08c6c
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-11 22:49:33 +0530

    Create Camelcase.py

---
+++ Camelcase.py
@@ -0,0 +1,10 @@
+str1 = input("")
+str2 =""
+for i in range(len(str1)):
+    if(i==0):
+        str2 +=str1[i].upper()
+    elif(str1[i-1].isspace()):
+        str2 +=str1[i].upper()
+    else:
+        str2 +=str1[i]
+print(str2)'
