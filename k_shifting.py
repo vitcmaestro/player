11e1f94c962e86bfc274d1574e3920a95efe570b
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-11 23:00:32 +0530

    Update k_shifting.py

--- k_shifting.py
+++ k_shifting.py
@@ -1,7 +1,11 @@
 n,k = map(int,input().split())
 lis = list(map(int,input().split()))
 if(n<k):
-    print(lis)
+    for i in range(n):
+        if(i == n-1):
+            print(lis[i],end="")
+        else:
+            print(lis[i],end=" ")
 else:
     x= n-k
     for i in range(x,n):
