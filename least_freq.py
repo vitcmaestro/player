1aa30ca2daad63acef86b86e1f789682ecc8db74
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-16 12:03:27 +0530

    Update least_freq.py

--- least_freq.py
+++ least_freq.py
@@ -1,5 +1,6 @@
+import collections
 temp = input("")
-s = temp.lower().strip()
+s = temp.lower().replace(" ","")
 s1 = []
 for i in s:
     if i not in s1:
@@ -8,6 +9,9 @@ count =[]
 for i in range(len(s1)):
     count.append(s.count(s1[i]))
 key = min(count)
+for i in range(len(s1)):
+    if s1[i] not in temp and s1[i].upper() in temp:
+        s1[i] = s1[i].upper()
 c= 0
 for i in range(len(count)):
     if(count[i] == key):
