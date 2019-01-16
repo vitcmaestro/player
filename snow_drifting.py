1fac613b367a232a685ca3e433a526e2874d92ef
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-16 12:39:27 +0530

    Update snow_drifting.py

--- snow_drifting.py
+++ snow_drifting.py
@@ -5,7 +5,7 @@ for i in range(n):
     lis.append(temp)
 lis.sort(key = lambda x: x[0])
 c =0
-for i in range(len(lis)):
+for i in range(len(lis)-1):
     if(lis[0][i] > lis[0][i+1]):
         c+=1
 print(c)
