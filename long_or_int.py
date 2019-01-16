e1d81d5240635f18a8de47109f877d6346d6b62f
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-16 17:41:06 +0530

    Update long_or_int.py

--- long_or_int.py
+++ long_or_int.py
@@ -2,5 +2,7 @@ x= input("")
 num = int(x.replace(",",""))
 if(num>= -2147483648 and num<= 2147483647):
     print("INT")
+elif(num>=9223372036854775808 and num<= 9223372036854775807):
+    print("LONG LONG")
 else:
     print("LONG")
