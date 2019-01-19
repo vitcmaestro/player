8ee8458937bbc379f67a27aef14847c30eb2d81a
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-19 20:56:29 +0530

    Create oddfactor.py

---
+++ oddfactor.py
@@ -0,0 +1,9 @@
+num = int(input(""))
+c =0
+for i in range(1,num+1):
+    if(num%i == 0):
+        if(c==0):
+            print(i,end = "")
+            c +=1
+        else:
+            print("",i,end = "")
